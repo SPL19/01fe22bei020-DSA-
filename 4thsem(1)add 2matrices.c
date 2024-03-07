ee61de7d3119b5540832aadc563319b05edbae6c
Sanjana Lengade <131254489+SPL19@users.noreply.github.com>
2024-03-07 19:26:43 +0530

    Add files via upload

---
+++ 4thsem(1)add 2matrices.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[3][3],b[3][3],c[3][3],i,j;
+    printf("enter the elements for matrix 1");
+
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             scanf("%d",&a[i][j]);
+         }
+     }
+printf("enter the elements for matrix 2");
+
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+             scanf("%d",&b[i][j]);
+         }
+     }
+  printf(" the elements for matrix 1 is");
+
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+            printf("%d",a[i][j]);
+         }
+         printf("\n");
+     }
+    printf(" the elements for matrix 2 is");
+
+     for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+            printf("%d",b[i][j]);
+         }
+         printf("\n");
+     }
+      for(i=0;i<3;i++)
+     {
+         for(j=0;j<3;j++)
+         {
+            c[i][j]=a[i][j]+b[i][j];
+         }
+     }
+    printf("the matrix sum is:");
+      for(i=0;i<3;i++)
+      {
+        for(j=0;j<3;j++)
+      {
+          printf(" %d",c[i][j]);
+      }
+       printf("\n");
+     }
+
+ }
+
+
