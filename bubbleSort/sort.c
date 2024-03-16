59423a47a6c4a60fef577fe70e6d8effafa3dcc9
Sanjana Lengade <131254489+SPL19@users.noreply.github.com>
2024-03-16 18:35:09 +0530

    File Handling

---
+++ bubbleSort/sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define max 25
+void bubbleSort(int[], int);
+
+int main()
+{
+    int a[max];
+
+    FILE *input, *output;
+    input = fopen("input.txt", "r");
+    if (input == NULL)
+    {
+        perror("\nError in opening a file\n");
+        return 1;
+    }
+    int i = 0;
+    int j = 0;
+    while (!feof(input))
+    {
+        fscanf(input, "%d", &a[i]);
+        i++;
+    }
+    fclose(input);
+
+    bubbleSort(a, i); //  Sort the array
+
+    for (j = 0; j < i; j++)
+    {
+        printf("%d ", a[j]);
+    }
+
+    output = fopen("output.txt", "w");
+
+    for (j = 0; j < i; j++)
+    {
+        fprintf(output, "%d ", a[j]);
+    }
+
+    fclose(output);
+    return 0;
+}
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
